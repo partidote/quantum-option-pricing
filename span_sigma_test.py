637906fbd0c15efe060585b688bfb01821a21095
Aaditeya Tripathi <136434223+partidote@users.noreply.github.com>
2025-07-30 20:54:29 +0530

    Add files via upload
    Add this code block to your driver to generate plots of QOP vs BSM prices when span_sigma = 2 and span_sigma = 3

---
+++ span_sigma_test.py
@@ -0,0 +1,16 @@
+# volatility sweep with default span_sigma = 3
+    sweep_volatility_vs_price(
+        ticker= my_ticker,
+        expiry_days=my_days,
+        sigma_range=(0.1, 1.2),
+        num_sigma=12
+    )
+
+    # volatility sweep with span_sigma = 2
+    sweep_volatility_vs_price(
+        ticker= my_ticker,
+        expiry_days=my_days,
+        sigma_range=(0.1, 1.2),
+        num_sigma=12,
+        span_sigma=2
+    )
